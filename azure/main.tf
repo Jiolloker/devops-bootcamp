59054bdc5a1b900baf877a933e7b3f7dc03e88cc
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2023-05-02 21:23:29 -0300

    add variables file

--- azure/main.tf
+++ azure/main.tf
@@ -8,6 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.tenant_id
+    tenant_id = var.tenantID
     features {}
 }
 
