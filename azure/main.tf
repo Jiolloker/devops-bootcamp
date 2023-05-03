1ae78e9af6e72b75b709d8a73ab12412ae0f5e5f
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2023-05-02 21:34:58 -0300

    add project variable

--- azure/main.tf
+++ azure/main.tf
@@ -19,3 +19,29 @@ resource "azurerm_resource_group" "rg" {
     name     = "educacionit-devops-rg"
     location = "eastus"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.project}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.2.0.0/20"]
+
+  subnet {
+    name           = "default"
+    address_prefix = "10.2.1.0/24"  
+  }
+
+  subnet {
+    name           = "frontend"
+    address_prefix = "10.2.2.0/24"
+  }
+
+    subnet {
+        name = "backend"
+        address_prefix = "10.2.3.0/24"
+    }
+
+  tags = {
+    env = "prod"
+  }
+}
